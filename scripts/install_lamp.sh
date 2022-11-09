20244d3890f0d448def3c57e2251b00c3b8ea60f
Juan Ramón Rueda <rayseink_1694@hotmail.com>
2022-11-09 16:45:37 +0000

    Se añaden los scripts reestructados y funcionando al repositorio

---
+++ scripts/install_lamp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Muestra los comandos que se van a ejecutar y poder depurar
+set -x
+
+# Actualización de los repositorios instalados
+apt-get update
+
+# Actualización los programas de los repositorios instalados, no funciona correctamente por eso está comentado
+#apt-get upgrade -y
+
+
+#---------------------------------------------------------------------------------------------------------------
+# Instalación de servidor web Apache2
+apt-get install apache2 -y
+
+
+#---------------------------------------------------------------------------------------------------------------
+# Instalación del servidor MySQL
+apt-get install mysql-server -y
+
+
+#---------------------------------------------------------------------------------------------------------------
+# Instalación de PHP y varios módulos que necesitamos
+apt-get install php libapache2-mod-php php-mysql -y
---
+++ scripts/install_tools.sh
@@ -0,0 +1,115 @@
+#!bin/bash
+
+set -x
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# Declaración de variables locales
+
+# Variable para phpMyAdmin
+PHPMYADMIN_APP_PASSWORD=phpmyadmin_password
+
+# Variables para creación de base de datos con usuario y contraseña
+database_prestashop=DB_PrestaShop
+database_user=Usuario_PrestaShop
+database_password=Password_PrestaShop
+
+
+# Variables para TLS mediante Certbot
+certbot_email=tetz_dqhwr17@yutep.com
+certbot_domain=prestashop05jrrl.ddns.net
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# Instalación de Unzip para descomprimir en formato .zip
+apt-get install unzip -y
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# Preconfiguración e instalación de phpMyAdmin
+
+# Configuración de las respuestas de instalación desatendida de phpMyAdmin
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $PHPMYADMIN_APP_PASSWORD" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $PHPMYADMIN_APP_PASSWORD" | debconf-set-selections
+
+# Instalación de phpMyAdmin
+apt install phpmyadmin -y
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# Creación de la base de datos con usuario y contraseña para PrestaShop
+echo "DROP DATABASE IF EXISTS $database_prestashop;" | mysql -u root
+echo "CREATE DATABASE $database_prestashop CHARSET utf8mb4;" | mysql -u root
+echo "USE $database_prestashop;" | mysql -u root
+
+echo "CREATE USER IF NOT EXISTS '$database_user'@'%' IDENTIFIED BY '$database_password';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON $database_prestashop.* TO '$database_user'@'%';" | mysql -u root
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# Preparación de TLS
+
+# Instalación de core mediante snap
+snap install core
+
+snap refresh core
+
+# Borrado de instalación de Certbot si hubiese e instalación limpia del mismo
+apt-get remove certbot
+
+snap install --classic certbot
+
+# Creación de enlace simbólico a /usr/bin para ejecutar el comando certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+
+# Ejecución de Certbot para obtener TLS
+certbot --apache -m $certbot_email --agree-tos --no-eff-email -d $certbot_domain
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# Descarga de archivos de PrestaShop
+
+# Creación de directorio para PrestaShop
+mkdir -p /var/www/prestashop
+
+# Cambio del directorio que sirve por defecto los sitios web de Apache, tanto por HTTP como por HTTPS
+sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/prestashop|' /etc/apache2/sites-available/000-default.conf
+
+sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/prestashop|' /etc/apache2/sites-available/000-default-le-ssl.conf
+
+# Reinicio de Apache para que se apliquen los cambios anteriores
+systemctl restart apache2
+
+# Creación de directorio en el directorio temporal para almacenar los archivos que se generen en la descarga y descompresión
+mkdir -p /tmp/prestashop
+
+# Descarga del paquete completo de PrestaShop en /tmp/prestashop
+wget -P /tmp/prestashop https://github.com/PrestaShop/PrestaShop/releases/download/8.0.0/prestashop_8.0.0.zip
+
+# Descompresión de PrestaShop en el mismo directorio
+unzip /tmp/prestashop/prestashop_8.0.0.zip -d /tmp/prestashop
+
+# Descompresión de los archivos necesarios de PrestaShop en /var/www/html usando el modificador -d
+unzip /tmp/prestashop/prestashop.zip -d /var/www/prestashop
+
+# Borrado de todos los archivos y directorios que se han descomprimido en /tmp/prestashop
+rm -rf /tmp/prestashop
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# Comprobación de PHP para PrestaShop
+
+# Creación de directorio para la descarga del comprobador de PHP
+mkdir -p /tmp/php-ps-info
+
+# Descarga de PHP PS Info en /tmp/php-ps-info
+wget -P /tmp/php-ps-info https://github.com/PrestaShop/php-ps-info/archive/refs/tags/v1.1.zip
+
+# Descompresión de la descarga de PHP PS Info en /tmp/php-ps-info
+unzip /tmp/php-ps-info/v1.1.zip -d /tmp/php-ps-info
+
+# Se mueve el archivo phppsinfo.php a /var/www/prestashop
+mv /tmp/php-ps-info/php-ps-info-1.1/phppsinfo.php /var/www/prestashop
