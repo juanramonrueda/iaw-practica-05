da46683cf9cd17f6deac7f53d40a2646fe09e8aa
Juan Ramón Rueda Lao <rayseink_1694@hotmail.com>
2022-11-21 23:09:19 +0100

    Unificación de variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,31 +6,26 @@ set -x
 #----------------------------------------------------------------------------------------------------------------------------
 # Declaración de variables locales
 
-# Variables para Certbot
-certbot_email=tetz_dqhwr17@yutep.com
-certbot_domain=practicasiawjrrl.ddns.net
+email_certbot_prestashop=tetz_dqhwr17@yutep.com
+address_domain=practicasiawjrrl.ddns.net
 
-# Variables para PrestaShop
-prestashop_language=es
+prestashop_language_country=es
 prestashop_shop_name="Tienda PrestaShop JRRL"
 prestashop_activity=7
-country_code=es
 ssl_state=1
 admin_firstname="Juan Ramón"
 admin_lastname="Rueda Lao"
-admin_email_back_office=tetz_dqhwr17@yutep.com
 admin_password_back_office=@Admin123#
 database_mysql_server=127.0.0.1
 database_name=DB_PrestaShop
 database_user=Usuario_PrestaShop
 database_password=Password_PrestaShop
 database_prefix=P_S_
-ip_address_domain=practicasiawjrrl.ddns.net
 
 
 #----------------------------------------------------------------------------------------------------------------------------
 # Obtención de TLS
-certbot --apache -m $certbot_email --agree-tos --no-eff-email -d $certbot_domain
+certbot --apache -m $email_certbot_prestashop --agree-tos --no-eff-email -d $address_domain
 
 
 #----------------------------------------------------------------------------------------------------------------------------
@@ -73,21 +68,21 @@ cd /var/www/prestashop/install
 
 # Ejecución de la instalación de PrestaShop
 php index_cli.php \
-    --language=$prestashop_language \
+    --language=$prestashop_language_country \
     --name=$prestashop_shop_name \
     --activity=$prestashop_activity \
-    --country=$country_code \
+    --country=$prestashop_language_country \
     --ssl=$ssl_state \
     --firstname=$admin_firstname \
     --lastname=$admin_lastname \
-    --email=$admin_email_back_office \
+    --email=$email_certbot_prestashop \
     --password=$admin_password_back_office \
     --db_server=$database_mysql_server \
     --db_name=$database_name \
     --db_user=$database_user \
     --db_password=$database_password \
     --prefix=$database_prefix \
-    --domain=$ip_address_domain
+    --domain=$address_domain
 
 
 #----------------------------------------------------------------------------------------------------------------------------
